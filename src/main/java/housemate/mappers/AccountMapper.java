a179105847fb9666b8f27f8ba2e3227c5ee3004e
namdh03 <duonghoangnam503@gmail.com>
2025-06-22 04:54:32 +0000

    test(register): default account user

--- src/main/java/housemate/mappers/AccountMapper.java
+++ src/main/java/housemate/mappers/AccountMapper.java
@@ -20,7 +20,7 @@ import java.time.LocalDateTime;
 @Component
 public class AccountMapper {
 
-    final String DEFAULT_AVATAR = "https://scontent.fsgn2-9.fna.fbcdn.net/v/t1.15752-9/384469032_6609223889131065_8293022876449520388_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=ae9488&_nc_ohc=gjDXwSBmi3YAX-hNO9i&_nc_ht=scontent.fsgn2-9.fna&oh=03_AdTYPieo_8M2sWscLr-rykTpN-IAaBS8JRWarwlkJQpKdA&oe=6540E586";
+    final String DEFAULT_AVATAR = "housemate-api.namdh03.site/images/panda.png";
     final int MAX_PROFICIENCY_SCORE = 100;
     final int DEFAULT_RATING = 0;
 
